213be4f2cfa37c890a56d6f2890e7bf77b0f78d6
Swara711 <73833213+Swara711@users.noreply.github.com>
2022-05-11 19:46:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,15 +3,11 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
-var peter_pan = "";
 var frozen = "";
 var disney = "";
-harry_potter = "";
 scoreLeftWrist = 0;
 
 function preload(){
-    harry_potter = loadSound("music.mp3");
-    peter_pan = loadSound("peter_pan.mp3");
     frozen = loadSound("frozen.mp3");
     disney = loadSound("Disney_music.mp3");
 }
@@ -46,40 +42,6 @@ function draw(){
    }
 }
 
-function play(){
-    var name = document.getElementById("name").value;
-  
-  
-  if(name=="peter_pan"){
-    song = peter_pan;
-  } 
-  else if(name=="frozen"){
-    song = frozen;
-  }
-  else if(name=="disney"){
-    song = disney;
-  }
-  else if(name=="harry_potter"){
-    song = harry_potter;
-  }
-  song.play();
-  song.setVolume(1);
-  song.rate(1);
-}
-
-function pause(){
-    harry_potter.pause();
-    frozen.pause();
-    disney.pause();
-    peter_pan.pause();
-}
-
-function stop(){
-    harry_potter.stop();
-    frozen.stop();
-    disney.stop();
-    peter_pan.stop();
-}
 
 function gotPoses(results){
   if(results.length > 0){
